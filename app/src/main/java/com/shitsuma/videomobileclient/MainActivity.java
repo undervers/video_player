93c810cb744938405e5a3c6d77390b3fd98d8763
Oleg <undervers@mail.ru>
2014-08-14 00:48:35 +0300

    Refactor

--- app/src/main/java/com/shitsuma/videomobileclient/MainActivity.java
+++ app/src/main/java/com/shitsuma/videomobileclient/MainActivity.java
@@ -15,83 +15,54 @@ import android.widget.TextView;
 import android.widget.VideoView;
 
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.shitsuma.videomobileclient.http.HttpResponseHandler;
+import com.shitsuma.videomobileclient.model.ServerConst;
 import com.shitsuma.videomobileclient.model.VideoInfo;
+import com.shitsuma.videomobileclient.parsers.Parser;
+import com.shitsuma.videomobileclient.parsers.XhamsterParser;
 
 import org.apache.http.Header;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends Activity {
 
+    private Parser videoParser = new XhamsterParser();
+    private AsyncHttpClient httpClient = new AsyncHttpClient();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(this));
+        downloadVideosInfo(ServerConst.XHAMSTER_URL);
+    }
 
-        AsyncHttpClient httpClient = new AsyncHttpClient();
-        httpClient.get("http://xhamster.com", new AsyncHttpResponseHandler() {
-            ProgressDialog waitingDialog;
-
-            @Override
-            public void onStart(){
-                waitingDialog = ProgressDialog.show(MainActivity.this, "Loading data", "Waiting please...");
-            }
-
-            @Override
-            public void onFinish(){
-                waitingDialog.dismiss();
-            }
-
+    private void downloadVideosInfo(String pageUrl){
+        httpClient.get(pageUrl, new HttpResponseHandler(this) {
             @Override
             public void onSuccess(int i, Header[] headers, byte[] bytes) {
-                parseMainPage(new String(bytes));
-            }
-
-            @Override
-            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
-
+                List<VideoInfo> videos = videoParser.parseVideosList(new String(bytes));
+                createMainPage(videos);
             }
         });
-
     }
 
-    private void parseMainPage(String page){
-        List <VideoInfo> videos = new ArrayList <VideoInfo> ();
+    private void downloadVideoDetail(String detailUrl){
+        httpClient.get(detailUrl, new HttpResponseHandler(this) {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                String videoUrl = videoParser.parseVideoUrl(new String(bytes));
 
-        Document html = Jsoup.parse(new String(page));
-        Elements videosElems = html.getElementsByTag("div");
-
-        for(Element video : videosElems){
-
-            if(video.attr("class").equals("video")) {
-
-                String videoPageUrl =  video.getElementsByTag("a").first().attr("href");
-
-                Element imgElem = video.getElementsByTag("img").first();
-                String imageUrl = imgElem.attr("src");
-                String info = imgElem.attr("alt");
-
-                videos.add(new VideoInfo(info, imageUrl, videoPageUrl));
+                playVideo(videoUrl);
             }
-        }
-
-        createMainPage(videos);
+        });
     }
 
-    private void createMainPage(final List <VideoInfo> videos){
+    private void createMainPage(final List<VideoInfo> videos) {
         GridView view = new GridView(this);
         view.setNumColumns(5);
-        view.setAdapter(new ArrayAdapter <VideoInfo>(this, 0, videos){
+        view.setAdapter(new ArrayAdapter<VideoInfo>(this, 0, videos) {
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
@@ -114,52 +85,24 @@ public class MainActivity extends Activity {
 
         view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView <?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                 VideoInfo video = videos.get(position);
-                parseVideoPage(video.getVideoPageUrl());
+                downloadVideoDetail(video.getVideoPageUrl());
             }
         });
-
-
 
         setContentView(view);
     }
 
-    private void parseVideoPage(String url){
-        AsyncHttpClient httpClient = new AsyncHttpClient();
-        httpClient.get(url, new AsyncHttpResponseHandler() {
-            ProgressDialog waitingDialog;
 
-            @Override
-            public void onStart(){
-                waitingDialog = ProgressDialog.show(MainActivity.this, "Loading data", "Waiting please...");
-            }
-
-            @Override
-            public void onFinish(){
-                waitingDialog.dismiss();
-            }
-
-            @Override
-            public void onSuccess(int i, Header[] headers, byte[] bytes) {
-                parseSecondPage(new String(bytes));
-            }
-
-            @Override
-            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
-
-            }
-        });
-    }
-
-    private void parseSecondPage(String html){
-        Document htmlDoc = Jsoup.parse(html);
-        String url = htmlDoc.getElementsByTag("video").attr("file");
+    private void playVideo(String videoUrl) {
 
         VideoView video = new VideoView(this);
-        video.setVideoURI(Uri.parse(url));
+        video.setVideoURI(Uri.parse(videoUrl));
 
         setContentView(video);
+
         video.start();
     }
 }
